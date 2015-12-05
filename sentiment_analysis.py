6fab7fe975a7dbbf0a5e1d4634c221f4d888f13d
David Nelson <davidmichael.nelson@gmail.com>
2015-12-05 17:09:07 -0200

    Load examples using AJAX + JS refactoring

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, jsonify
+from lib.classifier import Classifier
 
+print("#" * 80)
+print(" - Starting up application")
 app = Flask(__name__)
+print(" - Building new classifier - might take a while.")
+classifier = Classifier().build()
+print("#" * 80)
 
 @app.route('/')
 def main():
@@ -8,7 +14,17 @@ def main():
 
 @app.route('/predict')
 def predict():
-    return jsonify(q=request.args.get('q'), predicted_class=0.0, prediction=0.9541)
+    q = request.args.get('q')
+    label, prediction = classifier.classify(q)
+    return jsonify(q=q, predicted_class=label, prediction=prediction)
+
+@app.route('/examples')
+def examples():
+    examples = [
+        dict(q="teste 1", actual=1.0, predicted=0.0),
+        dict(q="teste 2", actual=1.0, predicted=1.0)
+    ]
+    return jsonify(items=examples)
 
 if __name__ == '__main__':
     app.run(debug=True)
