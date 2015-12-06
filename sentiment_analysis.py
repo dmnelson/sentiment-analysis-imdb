3a3884ca9edc4533bbfca0aa90c5d7af5e096acc
David Nelson <davidmichael.nelson@gmail.com>
2015-12-06 16:15:48 -0200

    Change approach to instantiate classifier to avoid boot timeouts

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,13 +2,20 @@ from flask import Flask, render_template, request, jsonify
 from lib.classifier import Classifier
 from lib.examples import Examples
 
-print("#" * 80)
 print(" - Starting up application")
 app = Flask(__name__)
 
-print(" - Building new classifier - might take a while.")
-classifier = Classifier().build()
-print("#" * 80)
+class App:
+    __shared_state = {}
+    def __init__(self):
+        self.__dict__ = self.__shared_state
+
+    def classifier(self):
+        if getattr(self, '_classifier', None) == None:
+            print(" - Building new classifier - might take a while.")
+            self._classifier = Classifier().build()
+            print(" - Done!")
+        return self._classifier
 
 @app.route('/')
 def main():
@@ -17,13 +24,13 @@ def main():
 @app.route('/predict')
 def predict():
     q = request.args.get('q')
-    label, prediction = classifier.classify(q)
+    label, prediction = App().classifier().classify(q)
     return jsonify(q=q, predicted_class=label, prediction=prediction)
 
 @app.route('/examples')
 def examples():
-    examples = Examples(classifier).load(5, 5)
+    examples = Examples(App().classifier()).load(5, 5)
     return jsonify(items=examples)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True)
